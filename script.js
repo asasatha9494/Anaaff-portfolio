a9d761f2a785046d7e4c0759ea337810493bb4b1
anaafsathath <anaaff.20240834@iit.ac.lk>
2026-05-23 17:01:07 +0530

    border radius smooth and addedactive navigation

--- script.js
+++ script.js
@@ -64,3 +64,30 @@ function type() {
 }
 
 type();
+
+const sections = document.querySelectorAll("section");
+const navLinks = document.querySelectorAll(".nav-links a");
+
+window.addEventListener("scroll", () => {
+
+  let current = "";
+
+  sections.forEach(section => {
+    const sectionTop = section.offsetTop;
+
+    if (scrollY >= sectionTop - 200) {
+      current = section.getAttribute("id");
+    }
+  });
+
+  navLinks.forEach(link => {
+
+    link.classList.remove("active");
+
+    if (link.getAttribute("href").includes(current)) {
+      link.classList.add("active");
+    }
+
+  });
+
+});
